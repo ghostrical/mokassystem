461c1856a62b58b745e9362b8cd709248a187950
seonhoshin <shinwder@gmail.com>
2023-08-11 00:37:42 +0900

    MOKA_COMMON_LOCATION 에서 추가시 PK 가 자동생성되도록 처리

--- src/main/java/com/moka/utils/CustomBeanUtils.java
+++ src/main/java/com/moka/utils/CustomBeanUtils.java
@@ -17,6 +17,9 @@ public class CustomBeanUtils<T> {
         final BeanWrapper src = new BeanWrapperImpl(source);
         final BeanWrapper dest = new BeanWrapperImpl(destination);
 
+        // BeanWrapper 은  Java 객체의 속성 값을 동적으로 읽거나 설정하는 기능을 제공하는 유틸리티 클래스입니다.
+        // 객체간 데이터 복사나 객체간 필드 체크할 때 쓴다고 한다.
+        // 여기서는 getPropertyValue, setPropertyValue 기능을 쓰려고 사용했다.
 
         for (final Field property : source.getClass().getDeclaredFields()) {
             Object sourceProperty = src.getPropertyValue(property.getName());
@@ -24,6 +27,11 @@ public class CustomBeanUtils<T> {
                 dest.setPropertyValue(property.getName(), sourceProperty);
             }
         }
+        // source.getClass().getDeclaredFields() 는 source에 해당하는 클래스(source.getClass()) 의 필드들(private int a, private int b 같은거)
+        // 뽑는다. 그런 getDeclaredFields 를 Field property 에 for 마다 하나씩 필드를 담음.
+        // 만약 MokaCommonLocation moka 일 때, moka.getClass().getDelaredFields() 는 [commonLocationCode, commonLocationName, commonLocationMapx, commonLocationMapy] 다.
+        // !(sourceProperty instanceof Collection<?>)는 sourceProperty 가 컬렉션타입인 경우 거른다는 거다.
+        // 예를 들어 class A { int a; List<String> b; } 라면 a는 통과하지만 b는 걸린다. b가 널이라면 널로 유지될 것이다.
 
         return destination;
     }
