ab3deca93b9e9141a130a3e09c641fb661827555
seonhoshin <shinwder@gmail.com>
2023-08-16 23:32:57 +0900

    feat JWT2 authority 종류 admin, client, supply, factory로 분할

--- src/main/java/com/moka/auth/utils/CustomAuthorityUtils.java
+++ src/main/java/com/moka/auth/utils/CustomAuthorityUtils.java
@@ -14,17 +14,28 @@ public class CustomAuthorityUtils {
    // @Value("${mail.address.admin}")
     private String adminPersnlTypeCode = "PTA001";
 
-    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER");
-    private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_USER");
-    private final List<String> ADMIN_ROLES_STRING = List.of("ADMIN", "USER");
-    private final List<String> USER_ROLES_STRING = List.of("USER");
+    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_CLIENT_USER", "ROLE_SUPPLY_USER", "ROLE_FACTORY_USER");
+    private final List<GrantedAuthority> CLIENT_USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_CLIENT_USER");
+    private final List<GrantedAuthority> SUPPLY_USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_SUPPLY_USER");
+    private final List<GrantedAuthority> FACTORY_USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_FACTORY_USER");
+
+    private final List<String> ADMIN_ROLES_STRING = List.of("ADMIN", "CLIENT_USER", "SUPPLY_USER", "FACTORY_USER");
+    private final List<String> CLIENT_USER_ROLES_STRING = List.of("CLIENT_USER");
+    private final List<String> SUPPLY_USER_ROLES_STRING = List.of("SUPPLY_USER");
+    private final List<String> FACTORY_USER_ROLES_STRING = List.of("FACTORY_USER");
 
     // 메모리 상의 Role을 기반으로 권한 정보 생성.
-    public List<GrantedAuthority> createAuthorities(String email) {
-        if (email.equals(adminPersnlTypeCode)) {
+    public List<GrantedAuthority> createAuthorities(String persnlTypeCode) {
+        if (persnlTypeCode.equals(adminPersnlTypeCode)) {
             return ADMIN_ROLES;
         }
-        return USER_ROLES;
+        if(persnlTypeCode.equals("PTC001")){
+            return CLIENT_USER_ROLES;
+        }
+        if(persnlTypeCode.equals("PTD0001")){
+            return SUPPLY_USER_ROLES;
+        }
+        return FACTORY_USER_ROLES;
     }
 
     // DB에 저장된 Role을 기반으로 권한 정보 생성
@@ -36,10 +47,17 @@ public class CustomAuthorityUtils {
     }
 
     // DB 저장 용
-    public List<String> createRoles(String email) {
-        if (email.equals(adminPersnlTypeCode)) {
+    public List<String> createRoles(String persnlTypeCode) {
+        if (persnlTypeCode.equals(adminPersnlTypeCode)) {
             return ADMIN_ROLES_STRING;
         }
-        return USER_ROLES_STRING;
+        if (persnlTypeCode.equals("PTC001")){
+            return CLIENT_USER_ROLES_STRING;
+        }
+        if (persnlTypeCode.equals("PTD001")){
+            return SUPPLY_USER_ROLES_STRING;
+        }
+        return FACTORY_USER_ROLES_STRING;
+
     }
 }
